625a6666a6c6dc34ac60fffee478a247e73ac600
wjhstd <>
2017-08-10 19:33:15 +0800

    javafx

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,20 +4,32 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        try {
+            // Read file fxml and draw interface.
+            Parent root = FXMLLoader.load(getClass()
+                    .getResource("myscene.fxml"));
+
+            primaryStage.setTitle("My Application");
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
     public static void main(String[] args) {
+
         launch(args);
+
     }
 }
